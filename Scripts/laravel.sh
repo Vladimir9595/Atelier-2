cd685a8f49082b1e9e03a1c53c7c48188634bbb2
Vladimir9595 <alss-sio-sisr21-svl@ccicampus.fr>
2022-12-18 00:09:33 +0100

    update laravel.sh

--- Scripts/laravel.sh
+++ Scripts/laravel.sh
@@ -1,60 +1,166 @@
 #!/bin/bash
 
-echo "---------------------";
+echo "----------------------";
 echo "Création d'une clé SSH";
-echo "---------------------";
+echo "----------------------";
 
 ssh-keygen -b 4096 -t rsa;
+# ssh-keygen -t ed25519;
 
 UpdateUpgrade () {
 
-    echo "---------------------";
+    echo "-------------------------";
 	echo "Mise à jour des libreries";
-	echo "---------------------";
+	echo "-------------------------";
 
     sudo apt update && sudo apt upgrade -y;
-
 };
 UpdateUpgrade;
 
+AddUser () {
+
+    echo "--------------------------";
+	echo "Ajout de l'user sauvegarde";
+	echo "--------------------------";
+
+    sudo adduser sauvegarde;
+};
+AddUser;
+
 Install () {
 
-	echo "---------------------";
+	echo "-----------------------------------------------------------";
 	echo "Installation d'Apache - PHP - MariaDB - phpMyAdmin - xdebug";
-	echo "---------------------";
+	echo "-----------------------------------------------------------";
 
-	sudo apt install apache2 php -y php-fpm -y mariadb-server -y mariadb-client -y phpmyadmin -y php-xdebug -y;
+	sudo apt install software-properties-common -y;
+    sudo add-apt-repository ppa:ondrej/php -y;
+    sudo apt update -y;
+
+    sudo apt install apache2 -y php8.2 -y php8.2-fpm -y php8.2-xml -y php8.2-curl -y php8.2-xdebug -y mariadb-server -y mariadb-client -y samba -y;
+    sudo apt install php8.2-{common,mysql,xml,xmlrpc,curl,gd,imagick,cli,dev,imap,mbstring,opcache,soap,zip,redis,intl,bcmath,fpm,ldap,bz2,pgsql,cgi} -y;
 
     sudo a2enmod proxy_fcgi setenvif;
-    sudo a2enconf php7.4-fpm;
+    sudo a2enconf php8.2-fpm;
 
     sudo systemctl restart apache2;
 
 };
 Install;
 
-NodeJS () {
+phpMyAdmin () {
 
-	echo "-------------------------------------";
-	echo "Update NodeJS lastest version v17.9.0";
-	echo "-------------------------------------";
+	echo "--------------------------------";
+	echo "Installation de phpMyAdmin 5.1.3";
+	echo "--------------------------------";
 
-	curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -;
+	echo "--------------------------------";
+	echo "Suppresion des anciens fichiers.";
+	echo "--------------------------------";
 
-	sudo apt-get install nodejs -y;
+	sudo rm -rf /usr/share/phpmyadmin;
+	sudo rm -rf /var/lib/phpmyadmin/tmp;
 
+	echo "------------------------------------------------------------------------";
+	echo "Récupération de la denière version de PMA, tar du dossier & suppression.";
+	echo "------------------------------------------------------------------------";
+
+	sudo wget https://files.phpmyadmin.net/phpMyAdmin/5.1.3/phpMyAdmin-5.1.3-english.tar.gz;
+	sudo tar -xf phpMyAdmin-5.1.3-english.tar.gz;
+	rm -rf phpMyAdmin-5.1.3-english.tar.gz;
+
+	echo "---------------------------------------------------------------------------------";
+	echo "Récupération de la denière version de PMA, tar du dossier & suppression terminés.";
+	echo "---------------------------------------------------------------------------------";
+
+	echo "----------------------------------------------------------------------";
+	echo "Création & déplacement de tous les fichiers dans /usr/share/phpmyadmin";
+	echo "----------------------------------------------------------------------";
+
+	sudo mkdir /usr/share/phpmyadmin;
+    sudo mv phpMyAdmin-5.1.3-english/* /usr/share/phpmyadmin/;
+
+	echo "----------------------------------------------------------------";
+	echo "Création des dossiers et changement des droits PMA dans /var/lib";
+	echo "----------------------------------------------------------------";
+
+	sudo mkdir -p /var/lib/phpmyadmin/tmp;
+	sudo chown -R www-data:www-data /var/lib/phpmyadmin;
+
+	echo "----------------------------------";
+	echo "Création du fichier config.inc.php";
+	echo "----------------------------------";
+
+	sudo cp /usr/share/phpmyadmin/config.sample.inc.php /usr/share/phpmyadmin/config.inc.php;
+
+	echo "-----------------------------------------------------------------";
+	echo "Suppression de l'ancien dossier et lancement de la configuration.";
+	echo "-----------------------------------------------------------------";
+
+	sudo rm -rf phpMyAdmin-5.1.3-english;
+
+	echo "-----------------------------------------";
+	echo "Installation de phpMyAdmin 5.1.3 terminée";
+	echo "-----------------------------------------";
+
+}
+phpMyAdmin;
+
+ConfigphpMyAdmin () {
+
+    echo "----------------------------------------";
+    echo "Configuration du fichier phpmyadmin.conf";
+    echo "----------------------------------------";
+
+    conf=/etc/apache2/conf-available/phpmyadmin.conf;
+
+    sudo touch ${conf};
+
+    echo "Alias /phpmyadmin /usr/share/phpmyadmin" | sudo tee -a ${conf};
+    echo "" | sudo tee -a ${conf};
+    echo "<Directory /usr/share/phpmyadmin>" | sudo tee -a ${conf};
+    echo "    Options Indexes FollowSymLinks" | sudo tee -a ${conf};
+    echo "    DirectoryIndex index.php" | sudo tee -a ${conf};
+    echo "" | sudo tee -a ${conf};
+    echo "    <IfModule mod_php5.c>" | sudo tee -a ${conf};
+    echo "        AddType application/x-httpd-php .php" | sudo tee -a ${conf};
+    echo "" | sudo tee -a ${conf};
+    echo "        php_flag magic_quotes_gpc Off" | sudo tee -a ${conf};
+    echo "        php_flag track_vars On" | sudo tee -a ${conf};
+    echo "        php_flag register_globals Off" | sudo tee -a ${conf};
+    echo "        php_value include_path ." | sudo tee -a ${conf};
+    echo "    </IfModule>" | sudo tee -a ${conf};
+    echo "" | sudo tee -a ${conf};
+    echo "</Directory>" | sudo tee -a ${conf};
+    echo "" | sudo tee -a ${conf};
+    echo "# Authorize for setup" | sudo tee -a ${conf};
+    echo "<Directory /usr/share/phpmyadmin/setup>" | sudo tee -a ${conf};
+    echo "    <IfModule mod_authn_file.c>" | sudo tee -a ${conf};
+    echo "    AuthType Basic" | sudo tee -a ${conf};
+    echo "    AuthName \"phpMyAdmin Setup\"" | sudo tee -a ${conf};
+    echo "    AuthUserFile /etc/phpmyadmin/htpasswd.setup" | sudo tee -a ${conf};
+    echo "    </IfModule>" | sudo tee -a ${conf};
+    echo "    Require valid-user" | sudo tee -a ${conf};
+    echo "</Directory>" | sudo tee -a ${conf};
+    echo "" | sudo tee -a ${conf};
+    echo "# Disallow web access to directories that don't need it" | sudo tee -a ${conf};
+    echo "<Directory /usr/share/phpmyadmin/libraries>" | sudo tee -a ${conf};
+    echo "    Order Deny,Allow" | sudo tee -a ${conf};
+    echo "    Deny from All" | sudo tee -a ${conf};
+    echo "</Directory>" | sudo tee -a ${conf};
+    echo "" | sudo tee -a ${conf};
+    echo "<Directory /usr/share/phpmyadmin/setup/lib>" | sudo tee -a ${conf};
+    echo "    Order Deny,Allow" | sudo tee -a ${conf};
+    echo "    Deny from All" | sudo tee -a ${conf};
+    echo "</Directory>" | sudo tee -a ${conf};
+
+    sudo a2enconf phpmyadmin;
+
+    echo "----------------------";
+    echo "Configuration terminée";
+    echo "----------------------";
 };
-NodeJS;
-
-NPM () {
-	echo "------------------";
-	echo "Update NPM lastest";
-	echo "------------------";
-
-	sudo npm install -g npm@latest;
-
-};
-NPM;
+ConfigphpMyAdmin;
 
 ConfigApache () {
 
@@ -70,47 +176,50 @@ ConfigApache () {
 
     sudo sed -i '29i\ ' ${default};
 
-    sudo sed -i '29i\           Include /etc/phpmyadmin/apache.conf' ${default};
+    sudo sed -i '30i\        Include /etc/apache2/conf-available/phpmyadmin.conf' ${default};
 
     sudo systemctl restart apache2;
 
     echo "--------------------------------------------------";
-    echo "Configuration d'un virtual host vers /var/www/Laravel_project";
+    echo "Configuration d'un virtual host vers /var/www/[your_folder]";
     echo "--------------------------------------------------";
 
-    sudo rm -rf /var/www/Laravel_project;
-	sudo mkdir /var/www/Laravel_project;
+    sudo rm -rf /var/www/[your_folder];
 
-    portfolio=/etc/apache2/sites-available/laravel.conf;
+    projet=/etc/apache2/sites-available/projetv1.conf;
 
-    sudo touch ${portfolio};
+    sudo touch ${projet};
 
-    echo "<VirtualHost *:80>" | sudo tee -a ${portfolio};
-    echo "" | sudo tee -a ${portfolio};
-	echo "          ServerName dev.laravel.com" | sudo tee -a ${portfolio};
-    echo "          ServerAdmin webmaster@localhost" | sudo tee -a ${portfolio};
-    echo "          DocumentRoot /var/www/Laravel_project/public/" | sudo tee -a ${portfolio};
-    echo "" | sudo tee -a ${portfolio};
-   	echo "          ErrorLog ${APACHE_LOG_DIR}/error.log" | sudo tee -a ${portfolio};
-	echo "          CustomLog ${APACHE_LOG_DIR}/access.log combined" | sudo tee -a ${portfolio};
-	echo "" | sudo tee -a ${portfolio};
-    echo "          <Directory /var/www/Laravel_project/public/>" | sudo tee -a ${portfolio};
-    echo "              Options Indexes FollowSymLinks MultiViews" | sudo tee -a ${portfolio};
-    echo "              AllowOverride All" | sudo tee -a ${portfolio};
-    echo "              Order allow,deny" | sudo tee -a ${portfolio};
-    echo "              allow from all" | sudo tee -a ${portfolio};
-    echo "              Require all granted" | sudo tee -a ${portfolio};
-    echo "          </Directory>" | sudo tee -a ${portfolio};
-	echo "" | sudo tee -a ${portfolio};
-    echo "          Include /etc/phpmyadmin/apache.conf" | sudo tee -a ${portfolio};
-    echo "" | sudo tee -a ${portfolio};
-    echo "</VirtualHost>" | sudo tee -a ${portfolio};
+    echo "<VirtualHost *:80>" | sudo tee -a ${projet};
+    echo "" | sudo tee -a ${projet};
+	echo "        ServerName dev.vladimir-projet.com" | sudo tee -a ${projet};
+    echo "        ServerAdmin webmaster@localhost" | sudo tee -a ${projet};
+    echo "        DocumentRoot /var/www/[your_folder]/public/" | sudo tee -a ${projet};
+    echo "" | sudo tee -a ${projet};
+   	echo "        ErrorLog \${APACHE_LOG_DIR}/error.log" | sudo tee -a ${projet};
+	echo "        CustomLog \${APACHE_LOG_DIR}/access.log combined" | sudo tee -a ${projet};
+	echo "" | sudo tee -a ${projet};
+    echo "        <Directory /var/www/[your_folder]/>" | sudo tee -a ${projet};
+    echo "              Options Indexes FollowSymLinks MultiViews" | sudo tee -a ${projet};
+    echo "              AllowOverride All" | sudo tee -a ${projet};
+    echo "              Order allow,deny" | sudo tee -a ${projet};
+    echo "              allow from all" | sudo tee -a ${projet};
+    echo "              Require all granted" | sudo tee -a ${projet};
+    echo "        </Directory>" | sudo tee -a ${projet};
+	echo "" | sudo tee -a ${projet};
+    echo "        Include /etc/apache2/conf-available/phpmyadmin.conf" | sudo tee -a ${projet};
+    echo "" | sudo tee -a ${projet};
+    echo "</VirtualHost>" | sudo tee -a ${projet};
 
-	sudo a2ensite laravel.conf;
+	sudo a2ensite projetv1.conf;
 
     sudo a2dissite 000-default.conf;
 
+    sudo a2enmod rewrite && sudo service apache2 restart;
+
 	sudo systemctl restart apache2;
+
+    sudo chown -R ubuntu:ubuntu /var/www/;
 
     echo "----------------------------------------";
     echo "Configuration terminée";
@@ -118,18 +227,90 @@ ConfigApache () {
 };
 ConfigApache;
 
-
 MariaDBConfiguration () {
 
-	sudo mariadb -e "DROP USER IF EXISTS 'vladimir'@'localhost';";
-	sudo mariadb -e "CREATE USER 'vladimir'@'localhost' IDENTIFIED BY 'Vladimir*1';";
-	sudo mariadb -e "CREATE DATABASE IF NOT EXISTS Portfolio;";
-	sudo mariadb -e "GRANT ALL ON *.* TO 'vladimir'@'localhost';";
-	sudo mariadb -e "FLUSH PRIVILEGES;";
+	sudo mariadb -e "DROP USER IF EXISTS '[pseudo]'@'localhost';";
+	sudo mariadb -e "CREATE USER '[pseudo]'@'localhost' IDENTIFIED BY '[your_password]';";
+    sudo mariadb -e "GRANT ALL ON *.* TO '[pseudo]'@'localhost';";
+    sudo mariadb -e "DROP USER IF EXISTS 'sauvegarde'@'localhost';";
+    sudo mariadb -e "CREATE USER 'sauvegarde'@'localhost' IDENTIFIED VIA unix_socket USING '***';";
+    sudo mariadb -e "GRANT SELECT, LOCK TABLES, SHOW VIEW ON *.* TO 'sauvegarde'@'localhost';";
+    sudo mariadb -e "DROP DATABASE IF EXISTS projet;";
+    sudo mariadb -e "CREATE DATABASE IF NOT EXISTS projet;";
+    sudo mariadb -e "FLUSH PRIVILEGES;";
+
 };
 MariaDBConfiguration;
 
-Composer () {
+
+# Voir fiche NVM https://linuxize.com/post/how-to-install-node-js-on-ubuntu-22-04/
+# Utilisation nvm ( Node Version Manager ).
+# Permet d'installer plusieurs versions de Node.js sur la même machine.
+
+Nodejs () {
+
+	echo "-------------------------------------";
+	echo "Update NodeJS lastest version v17.9.0";
+	echo "-------------------------------------";
+
+	curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -;
+
+	sudo apt-get install nodejs -y;
+    # Compiler des modules complémentaires natifs à partir de npm
+    sudo apt install build-essential -y;
+
+};
+Nodejs;
+
+NPM () {
+	echo "------------------";
+	echo "Update NPM lastest";
+	echo "------------------";
+
+	sudo npm install -g npm@latest;
+
+};
+NPM;
+
+# Installation de Node.js et npm à l'aide de NVM
+NodejsNPM (){
+    echo "----------------------------------------------";
+	echo "Installation de Node.js et npm à l'aide de NVM";
+	echo "----------------------------------------------";
+
+    wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+
+    # Close and reopen your terminal to start using nvm or run the following to use it now:
+
+    export NVM_DIR="$HOME/.nvm";
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh";  # This loads nvm
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion";  # This loads nvm bash_completion
+
+    nvm install node;
+
+    echo "--------------------------------------------------------";
+	echo "Installons deux autres versions, la dernière version LTS";
+	echo "--------------------------------------------------------";
+
+    nvm install --lts;
+    nvm install 14.19.3;
+
+    # echo "---------------------------------";
+    # echo "Affichons les versions installées";
+    # echo "---------------------------------";
+
+    # nvm ls
+
+    # echo "---------------------------------------";
+    # echo "Modifier la version actuellement active";
+    # echo "---------------------------------------";
+
+    # nvm use [version];
+
+}
+NodejsNPM;
+
+InstallComposer () {
 
 	echo "------------------------";
 	echo "Installation de Composer";
@@ -152,7 +333,24 @@ Composer () {
 	echo "Installation de Composer terminée";
 	echo "---------------------------------";
 };
-Composer;
+InstallComposer;
+
+# Composer(){
+
+#     echo "------------------";
+# 	echo "Install Composer";
+#     echo "------------------";
+
+
+#     sudo apt install php-cli unzip;
+
+#     curl -sS https://getcomposer.org/installer -o composer-setup.php;
+#     HASH=`curl -sS https://composer.github.io/installer.sig`;
+#     php -r "if (hash_file('SHA384', 'composer-setup.php') === '$HASH') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;";
+#     sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer;
+
+# }
+# Composer;
 
 xDebugConfiguration () {
 
@@ -164,11 +362,11 @@ xDebugConfiguration () {
 	# https://blog.pascal-martin.fr/post/xdebug-installation-premiers-pas/#installation-xdebug-linux
     # https://lucidar.me/fr/aws-cloud9/how-to-install-and-configure-xdebug-on-ubuntu/
 
-	php_ini=/etc/php/7.4/cli/php.ini;
-	xdebug_file=/etc/php/7.4/mods-available/xdebug.ini;
+	php_ini=/etc/php/8.2/cli/php.ini;
+	xdebug_file=/etc/php/8.2/mods-available/xdebug.ini;
 
-	echo "display_errors = On" | sudo tee -a ${php_ini};
-	echo "html_errors = On" | sudo tee -a ${php_ini};
+	sudo sed -i '503i\display_errors = On' | sudo tee -a ${php_ini};
+	sudo sed -i '556i\html_errors = On' | sudo tee -a ${php_ini};
 
 	sudo systemctl restart apache2;
 
@@ -179,25 +377,34 @@ xDebugConfiguration () {
 };
 xDebugConfiguration;
 
-Laravel () {
-    echo "--------------------------------";
-	echo "Installation Laravel";
-	echo "--------------------------------";
+SambaConfiguration () {
 
-    sudo composer create-project laravel/laravel /var/www/Laravel_project_test;
-    sudo chown ubuntu:ubuntu /var/www/Laravel_project_test && sudo chown ubuntu:ubuntu /var/www/Laravel_project_test/*;
-    cd /var/www/Laravel_project_test;
-    sudo cp .env.example .env;
-    sudo php artisan key:generate;
-    sudo php artisan cache:clear;
+	echo "-------------------------------------------------------------";
+	echo "Ajout de la configuration du partage dans /etc/samba/smb.conf";
+	echo "-------------------------------------------------------------";
 
-    echo "--------------------------------";
-	echo "Configuration de xDebug terminée";
-	echo "--------------------------------";
+    smbconf=/etc/samba/smb.conf;
+
+	echo '[Projet]' | sudo tee -a ${smbconf};
+	echo '   comment = Partage Projet' | sudo tee -a ${smbconf};
+	echo '   path = /var/www/[your_folder]/' | sudo tee -a ${smbconf};
+	echo '   read only = no' | sudo tee -a ${smbconf};
+	echo '   browseable = yes' | sudo tee -a ${smbconf};
+
+    echo "-----------------------------------------";
+	echo "SAISISSEZ LE MOT DE PASSE DU COMPTE SAMBA";
+	echo "-----------------------------------------";
+
+	sudo smbpasswd -a ubuntu; # ajouter l'user : définir mdp
+	sudo service smbd restart;
+    # supprimer l'user : sudo smbpasswd -x [nom_user];
+
+	echo "----------------------------------------------";
+	echo "Utilisateur ubuntu ajouté aux partages Samba";
+	echo "----------------------------------------------";
 
 };
-Laravel;
-
+SambaConfiguration;
 
 UpdateUpgrade () {
 
@@ -225,4 +432,71 @@ clean () {
 	sudo apt autopurge -y;
 
 };
-clean
+clean;
+
+# ---------------------------------------------------------------------------------------
+# Commandes pour Laravel
+# Laravel () {
+#     echo "--------------------------------";
+#     echo "Installation Laravel";
+# 	  echo "--------------------------------";
+
+#     1st option => Créer un projet de 0
+
+#     echo "--------------------------------";
+#     echo "Créer un projet de 0";
+# 	  echo "--------------------------------";
+
+#     cd /var/www;
+#     composer create-project laravel/laravel [your_folder];
+#     sudo chown -R ubuntu:ubuntu /var/www/[your_folder];
+#     cd /var/www/[your_folder];
+
+#     2nd option
+#     echo "-----------------------";
+#     echo "Cloner le projet depuis github";
+#     echo "-----------------------";
+
+#     cd /var/www;
+#     git clone [lien_git_http];
+#     sudo chown -R ubuntu:ubuntu /var/www/[your_folder];
+#     cd /var/www/[your_folder];
+
+#     Si vous pouvez, cloner le projet depuis une seule branche;
+#     git clone --single-branch --branch [nom_branche] [lien_git_http];
+#     cd /var/www/[your_folder];
+
+
+# -- Après avoir installé ou cloné le projet
+#     composer install;
+#     npm install;
+# -- Si severity vulnerability
+#     npm audit fix
+
+#     cp .env.example .env;
+#     nano .env (mettre les infos de la BDD - mail);
+#     php artisan key:generate;
+#     php artisan cache:clear;
+
+
+
+# -- Mettre à jour la base de données
+#     php artisan migrate;
+#     php artisan migrate:refresh;
+
+# -- Si Msg d'erreur après composer install modifier dans le fichier composer.json le :
+# "autoload": {
+#        "psr-4": {
+#            "App\\": "app/",
+#            "Database\\Factories\\": "database/factories/",
+#            "Database\\Seeders\\": "database/seeders/"
+#        }
+#    }
+
+
+#    sudo chown -R ubuntu:www-data /var/www/[your_folder]/bootstrap/
+#    sudo chown -R ubuntu:www-data /var/www/[your_folder]/public/
+#    sudo chown -R www-data:www-data /var/www/[your_folder]/storage/
+
+# };
+# Laravel;
